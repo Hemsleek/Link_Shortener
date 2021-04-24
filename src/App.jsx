ff430011ba8d0d16bd364826cc475b968c749671
hemsleek <asiyanbimubashir@gmail.com>
2021-04-24 11:54:05 +0100

    update: shortener handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import {useState, useRef} from 'react'
 import './App.css';
 
 function App() {
+  const [shortLink, setShortLink] = useState({})
   const accessToken = process.env.REACT_APP_ACCESS_TOKEN
-
+  const urlOutput = useRef('')
 
   const shortenLink = (e) => {
       e.preventDefault()
@@ -16,7 +18,11 @@ function App() {
         },
         body:JSON.stringify({'long_url':longLink,'domain':'bit.ly'})
       })
-      .then(response => response.json()).then(data => console.log(data))
+      .then(response => response.json()).then(data => {
+        setShortLink(data)
+        urlOutput.current = shortLink.link
+      })
+      .catch(console.log)
   }
 
   return (
@@ -24,6 +30,7 @@ function App() {
       <form onSubmit={shortenLink} >
         <input type="text"
           placeholder='Enter Url Here'
+          ref={urlOutput}
           name='urlInput'
           className='url-input'
         />
@@ -32,7 +39,10 @@ function App() {
       <div className="display-area">
         <input type="text"
           placeholder="shorted Link"
+          value={Object.keys(shortLink)? shortLink.link : ''}
+          
           className='url-output'
+          readOnly
         />
         <button className="copy-button">
           Copy Link
